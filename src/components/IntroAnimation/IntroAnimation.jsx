05f9a1f457d47cc481483b75d34b4482555b44da
David Sheinbein <davesheinbein@gmail.com>
2020-08-17 17:41:45 -0700

    update to css add link from home

--- src/components/IntroAnimation/IntroAnimation.jsx
+++ src/components/IntroAnimation/IntroAnimation.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Goo from '../Goo/Goo';
 import './styles/IntroAnimation.css';
+import { Link } from 'react-router-dom';
 
 function IntroAnimation() {
 	return (
@@ -20,7 +21,9 @@ function IntroAnimation() {
 					<span>CALCULATOR</span>
 				</h1>
 			</section>
-            <Goo/>
+			<Link to='/main' className='NavLink'>
+				<Goo />
+			</Link>
 		</div>
 	);
 }
