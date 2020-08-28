9e1e09c5567cc664e9fdedeae99307a08e757a5e
David Sheinbein <davesheinbein@gmail.com>
2020-08-28 13:36:24 -0700

    update to eval action

--- src/reducers/calculationReducer.js
+++ src/reducers/calculationReducer.js
@@ -1,9 +1,20 @@
 export const calculationReducer = (state = '0', action) => {
 	let newState = action.text;
+	console.log('action.text', action.text);
 	switch (action.type) {
 		case 'EVALUATE':
-			// Evaluate the final expression and return the result to store
-			return this.eval(newState).toString();
+			try {
+				// Evaluate the final expression and return the result to store
+				return eval(newState).toString();
+			} catch (error) {
+				console.log('Error >>>> ' + error);
+				console.log('Error.stack >>>> ' + error.stack);
+				console.log('Error.type >>>> ' + error.type);
+				alert(
+					`Invalid Syntax - Please try again - No hanging brackets ) ( or improper use of decimal points . allowed - Rest to zero`
+				);
+				return (state = '0');
+			}
 		case 'DISPLAY':
 			// Save and display the text of the action to store
 			return newState;
